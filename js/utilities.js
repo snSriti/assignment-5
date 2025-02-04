ebfe604fdf5547fb778586207727d79a42210031
Sriti <sadikunnaher41@gmail.com>
2025-02-04 22:59:48 +0600

    next commit

--- js/utilities.js
+++ js/utilities.js
@@ -10,13 +10,11 @@ function getTextValueById(id){
 }
 
 function showSection(id){
-    console.log(id);
 document.getElementById('donation-body').classList.remove('hidden');
 document.getElementById('history-body').classList.add('hidden');
 
 }
 function showTheSection(id){
-    console.log(document.getElementById('history-body'));
 document.getElementById('donation-body').classList.add('hidden');
 document.getElementById('history-body').classList.remove('hidden');
 
